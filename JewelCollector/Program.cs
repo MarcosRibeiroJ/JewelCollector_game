47693ecc052f31f1568bf14b6f5713afe0e16968
Marcos de Souza Ribeiro Junior <marcossrj90@gmail.com>
2023-05-28 19:20:46 -0300

    Segunda alteracao realizada por marco

--- JewelCollector/Program.cs
+++ JewelCollector/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //Alteração realizada por Marcos Ribeiro com usuário marco
+//Outra alteração realizada por Marcos Ribeiro com usuário marco
