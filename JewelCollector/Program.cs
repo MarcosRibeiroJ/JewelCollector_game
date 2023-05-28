5c98ccf747993d74074bc67252c9991b9286a8dc
Marcos Ribeiro <marcossrj90@gmail.com>
2023-05-28 19:21:45 -0300

    Segunda alteracao mribeiro

--- JewelCollector/Program.cs
+++ JewelCollector/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //Alteração realizada por Marcos ribeiro com usuário mribeiro
+//Outra alteração realizada por Marcos ribeiro com usuário mribeiro
